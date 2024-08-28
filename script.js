5adb632a97f45ed58a864ddb12ea7e1a36d77fce
Mufaddal KC <mufazzalkc@gmail.com>
2024-08-28 10:28:16 +0530

    Update js code

--- script.js
+++ script.js
@@ -1,51 +1,109 @@
+// Updateted code
 let screen = document.getElementById("screen");
-let currentNumber = "";
-let previousNumber = "";
-let operator = "";
 
 function appendNum(num) {
-  currentNumber = currentNumber + num;
-  screen.value = currentNumber;
+  screen.value += num;
 }
 
 function appendOp(op) {
-  operator = op;
-  previousNumber = currentNumber;
-  currentNumber = "";
-  screen.value = previousNumber + op;
+  screen.value += op;
 }
 
-function calclate() {
-  let result = 0;
-  switch (operator) {
-    case "+":
-      result = parseFloat(previousNumber) + parseFloat(currentNumber);
-      break;
-    case "-":
-      result = parseFloat(previousNumber) - parseFloat(currentNumber);
-      break;
-    case "*":
-      result = parseFloat(previousNumber) * parseFloat(currentNumber);
-      break;
-    case "/":
-      result = parseFloat(previousNumber) / parseFloat(currentNumber);
-      break;
-  }
-
-  screen.value = result;
-  currentNumber = result;
-  previousNumber = "";
-  operator = "";
+function calclate(e) {
+  e.preventDefault();
+  screen.value = eval(screen.value);
 }
 
 function resetScreen() {
   screen.value = "";
-  currentNumber = "";
-  previousNumber = "";
-  operator = "";
 }
 
 function deleteItem() {
-  currentNumber = currentNumber.slice(0, -1);
-  screen.value = currentNumber;
+  screen.value = screen.value.slice(0, -1);
 }
+
+function onKeyDown(e) {
+  console.log(e.key);
+  if (e.key === "Backspace") {
+    deleteItem();
+  } else if (
+    ![
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "0",
+      "+",
+      "-",
+      "*",
+      "/",
+      "=",
+      "Enter",
+      "(",
+      ")",
+      "ArrowLeft",
+      "ArrowRight",
+    ].includes(e.key)
+  ) {
+    e.preventDefault();
+  }
+}
+
+// **Old code
+
+// let screen = document.getElementById("screen");
+// let currentNumber = "";
+// let previousNumber = "";
+// let operator = "";
+
+// function appendNum(num) {
+//   currentNumber = currentNumber + num;
+//   screen.value = currentNumber;
+// }
+
+// function appendOp(op) {
+//   operator = op;
+//   previousNumber = currentNumber;
+//   currentNumber = "";
+//   screen.value = previousNumber + op;
+// }
+
+// function calclate() {
+//   let result = 0;
+//   switch (operator) {
+//     case "+":
+//       result = parseFloat(previousNumber) + parseFloat(currentNumber);
+//       break;
+//     case "-":
+//       result = parseFloat(previousNumber) - parseFloat(currentNumber);
+//       break;
+//     case "*":
+//       result = parseFloat(previousNumber) * parseFloat(currentNumber);
+//       break;
+//     case "/":
+//       result = parseFloat(previousNumber) / parseFloat(currentNumber);
+//       break;
+//   }
+
+//   screen.value = result;
+//   currentNumber = result;
+//   previousNumber = "";
+//   operator = "";
+// }
+
+// function resetScreen() {
+//   screen.value = "";
+//   currentNumber = "";
+//   previousNumber = "";
+//   operator = "";
+// }
+
+// function deleteItem() {
+//   currentNumber = currentNumber.slice(0, -1);
+//   screen.value = currentNumber;
+// }
